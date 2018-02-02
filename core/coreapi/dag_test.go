d5e7ecf82e975083700cd342f2cc24d6a46c2ee4
Łukasz Magiera <magik6k@gmail.com>
2018-02-02 16:00:37 +0100

    coreapi: expand public path api
    License: MIT
    Signed-off-by: Łukasz Magiera <magik6k@gmail.com>

--- core/coreapi/dag_test.go
+++ core/coreapi/dag_test.go
@@ -6,8 +6,6 @@ import (
 	"strings"
 	"testing"
 
-	coreapi "github.com/ipfs/go-ipfs/core/coreapi"
-
 	mh "gx/ipfs/QmZyZDi491cCNTLfAhwcaDii2Kg4pwKRkhqQzURGDvY6ua/go-multihash"
 
 	opt "github.com/ipfs/go-ipfs/core/coreapi/interface/options"
@@ -74,7 +72,7 @@ func TestPath(t *testing.T) {
 		t.Error(err)
 	}
 
-	p, err := coreapi.ParsePath(path.Join(res.Cid().String(), "lnk"))
+	p, err := api.ParsePath(ctx, path.Join(res.Cid().String(), "lnk"))
 	if err != nil {
 		t.Error(err)
 	}
--- core/coreapi/interface/coreapi.go
+++ core/coreapi/interface/coreapi.go
@@ -5,6 +5,9 @@ package iface
 import (
 	"context"
 
+	options "github.com/ipfs/go-ipfs/core/coreapi/interface/options"
+
+	cid "gx/ipfs/QmcZfnkapfECQGcLZaf9B79NRg7cRa9EnZh4LSbkCzwNvY/go-cid"
 	ipld "gx/ipfs/Qme5bWv7wtjUNGsK2BNGVUFPKiuxWrsqrtvYwCLRw8YFES/go-ipld-format"
 )
 
@@ -37,4 +40,14 @@ type CoreAPI interface {
 	// ResolveNode resolves the path (if not resolved already) using Unixfs
 	// resolver, gets and returns the resolved Node
 	ResolveNode(context.Context, Path) (ipld.Node, error)
+
+	// ParsePath parses string path to a Path
+	ParsePath(context.Context, string, ...options.ParsePathOption) (Path, error)
+
+	// WithResolve is an option for ParsePath which when set to true tells
+	// ParsePath to also resolve the path
+	WithResolve(bool) options.ParsePathOption
+
+	// ParseCid creates new path from the provided CID
+	ParseCid(*cid.Cid) Path
 }
